7968c9fdcc2a1e8c69c06ab5e458e32126915a6e
shubham <shubhamkumar7640@gmail.com>
2024-05-21 08:44:49 +0530

    Initial commit

---
+++ src/main/java/com/springReact/DocumentManager/DocumentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.springReact.DocumentManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springReact/DocumentManager/DocumentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springReact.DocumentManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
